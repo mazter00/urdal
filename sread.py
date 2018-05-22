ffad776e47d0041c804c2d24922dd9ecd6d20c8c
MortenAlnius <37439106+MortenAlnius@users.noreply.github.com>
2018-05-22 15:30:28 +0200

    v0.010
    Added "blocking" debug, disabled by default. Was supposed to give a list of "blocked" temps...

--- sread.py
+++ sread.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 '''
+v0.010 22.05.2018 15:29 Added "blocking" debug, disabled by default. Was supposed to give a list of "blocked" temps...
+v0.009 22.05.2018 Tried to reduce some output
 v0.008 09.05.2018 - Now autosaves temp.log once per hour
 v0.007 08.05.2018 11:43 - With a little bit of color
 v0.006 08.05.2018: Finds avaible devtty on its own
@@ -9,6 +11,10 @@ v0.005 07.05.2018: Now *runs* script plott.py and ftp.py using os.system
 v0.004 - 02.05.2018 - Now imports plotting if >10 minutes, and imports ftp.py if over 15 minutes
 v0.003 - 02.05.2018 - Minsket endringer for lenge siden
 v0.002 - 12.04.2018 - ...
+'''
+'''
+TODO:
+Show debug outputs of PONG
 '''
 
 from datetime import datetime
@@ -40,6 +46,10 @@ tempfile = open('temp.log', 'a')
 tsplotting = 0
 tsftp = 0
 tsautosave = 0
+catchb = False
+blocked = None
+# Debug Outtemp? True/False
+blokking = False
 
 while True:
 	# Arduino = 20, Python = .21
@@ -47,16 +57,9 @@ while True:
 	
 	if sport.inWaiting() > 0:
 		a = sport.readline().rstrip()
-		
 	else:
 		a = b""
 		len = 0
-	
-	# debug 
-	debug = 1
-	if (debug == 1) and a: 
-		if "DEBUG Compare" not in str(a,'utf-8'):
-			print(Style.DIM+"Debug i python: "+str(a,'utf-8'))
 	
 	current = int(time.time())
 	
@@ -73,10 +76,18 @@ while True:
 		
 		tempd = temp.decode('utf-8')
 		
+		# Try Boolean, True if it can be typecast to float
+		tryb = False
+		
+		try: 
+			float(tempd)
+			tryb = True
+		except:
+			print(Style.BRIGHT+Fore.RED+"Typecasting to float failed, temperature may be invalid")
+			tryb = False
+			
+		
 		d = datetime.now().isoformat()
-		# print(d)
-		# print("Skrevet til fil: "+d+' '+tempd)
-		print(Style.DIM+d+' '+Back.WHITE+Fore.BLUE+" "+str(tempd)+" ")
 		
 		savediff = current-tsautosave
 		if (savediff >= 3600):
@@ -85,10 +96,47 @@ while True:
 			tempfile = open('temp.log', 'a')
 			tsautosave = current
 			
-		tempfile.write(d+' '+tempd+'\n')
+		if (tryb is True):
+			txt = Style.DIM+d+' '+Back.WHITE+Fore.RED+" "+str(tempd)+" "
+			if blocked and blokking:
+				txt = txt+Style.RESET_ALL+Style.DIM+" Blocked: "+blocked
+			print(txt)
+			tempfile.write(d+' '+tempd+'\n')
+			tryb = False
+			catchb = True
 		tempfile.flush()
 		# print(type(tempd))
 		
+		
+	# Se etter "outtemp"
+	if blokking is True:
+		if b"Outtemp" in a:
+			print("Outtemp found in a: "+str(a))
+					
+			c = a.decode('utf-8')
+			c = c.split(' ')
+			# print("x av c: "+str(len(c)))
+			
+			b1,b2 = c[2],c[4]
+			out1,out2,out3 = c[-5],c[-3],c[-1]
+			
+			if (b1 == 0):
+				blocked = str(out2)+" og "+str(out3)
+			else: 
+				print(c)
+				print("b1 og b2: ",str(b1),str(b2))
+				print("out1, 2 og 3: ",str(out1),str(out2),str(out3))
+				# blocked = str(out1)+" "+str(out2)
+	
+	# debug 
+	if (catchb is False):
+		debug = 1
+		if (debug == 1) and a: 
+			# if "DEBUG Compare" not in str(a,'utf-8'):
+			if b"DEBUG Compare" not in a:
+				print(Style.DIM+"Partial line, Debug i python: "+str(a,'utf-8'))
+
+	
 	# TODO: Lete etter feilmeldinger fra Arduino.
 
 	diff = current-tsplotting
